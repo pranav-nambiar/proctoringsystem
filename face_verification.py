a8a5b1273bcce05069a30a08e3534088fdbb6706
pranav-nambiar <46402590+pranav-nambiar@users.noreply.github.com>
2021-09-15 11:10:59 +0530

    Add files via upload

---
+++ face_verification.py
@@ -0,0 +1,114 @@
+from deepface import DeepFace
+import cv2
+import threading
+import queue
+from multiprocessing import Queue
+import os
+
+
+class VideoCapture:
+    def __init__(self,name):
+        self.capture=cv2.VideoCapture(name)
+        self.q=queue.Queue()
+        t=threading.Thread(target=self._reader)
+        t.daemon=True
+        t.start()
+
+    def _reader(self):
+        while True:
+            ret, frame = self.capture.read()
+            if not ret:
+                print("No frames captured, exiting")
+                break
+            if not self.q.empty():
+                try:
+                    self.q.get_nowait()
+                except Queue.Empty:
+                    pass
+            self.q.put(frame)
+
+    def read(self):
+        return self.q.get()
+
+    def release (self):
+        self.capture.release()
+
+
+
+
+# capture = cv2.VideoCapture(0)
+# capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+
+# def verifyFace():
+#     capture = VideoCapture(0)
+#     i = 0
+#     while(True):
+#         # ret,image = capture.read()
+#         image = capture.read()
+#         try:
+#             found = 0
+#             print("Comparing for frame " + str(i))
+#             with os.scandir("database/pranav") as it:
+#                 for entry in it:
+#                     if entry.name.endswith(".jpg") and entry.is_file():
+#                         result = DeepFace.verify(entry.path, image, detector_backend = 'opencv', model_name = "ArcFace")
+#                         if result['verified']:
+#                             found = 1
+#             if found:
+#                 print("Succesful")
+#                 capture.release()
+#                 cv2.destroyAllWindows()
+#                 return True
+#             else:
+#                 print("Retrying.")
+
+#         except ValueError:
+#             print("Face not detected. Retrying...")
+#         cv2.imshow('frame',image)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#                 break
+#     capture.release()
+#     cv2.destroyAllWindows()
+
+
+
+
+def verifyFace():
+    capture = VideoCapture(0)
+    frames = 0
+    count = 0
+    while(True):
+        # ret,image = capture.read()
+        image = capture.read()
+        try:
+            print("Comparing for frame " + str(frames))
+            # with os.scandir("database/pranav") as it:
+            #     for entry in it:
+            #         if entry.name.endswith(".jpg") and entry.is_file():
+            #             result = DeepFace.verify(entry.path, image, detector_backend = 'opencv', model_name = "ArcFace")
+            #             if result['verified']:
+            #                 found = 1
+
+            df = DeepFace.find(image,'database/pranav',model_name='ArcFace',detector_backend='opencv')
+            if len(df):
+                count += 1
+
+            if count==5:
+                print("FACE VERIFIED FIVE TIMES SUCCESSFULLY!")
+                capture.release()
+                cv2.destroyAllWindows()
+                return True
+            frames += 1
+
+        except ValueError:
+            print("Face not detected. Retrying...")
+        
+        if frames==20:
+            print("You're an impostor")
+            return False
+        cv2.imshow('frame',image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    capture.release()
+    cv2.destroyAllWindows()
---
+++ ibmwatson2.py
@@ -0,0 +1,104 @@
+'''
+Speech to text transcription, from your mike, in real-time, using IBM Watson.
+'''
+
+# import argparse
+import time
+
+import fluteline
+
+import watson_streaming
+import watson_streaming.utilities
+
+import string
+
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+
+
+def read_tokenize_return(data):
+	filtered = []
+	stop_words = set(stopwords.words('english'))   
+	word_tokens = word_tokenize(data.translate(dict.fromkeys(string.punctuation))) ######### tokenizing sentence
+	for w in word_tokens:   ####### Removing stop words
+	    if w not in stop_words: 
+	        filtered.append(w.lower())
+	return set(filtered)
+
+def ret_qs():
+	##### checking whether proctor needs to be alerted or not
+	if not ret_qs.filtered_questions:
+		file = open("paper.txt") ## Question file
+		data = file.read()
+		file.close()
+		# stop_words = set(stopwords.words('english'))   
+		# word_tokens = word_tokenize(data) ######### tokenizing sentence
+		# for w in word_tokens:   ####### Removing stop words
+		#     if w not in stop_words: 
+		#         ret_qs.filtered_questions.append(w)
+		ret_qs.filtered_questions = read_tokenize_return(data)
+	return ret_qs.filtered_questions 
+
+ret_qs.filtered_questions = set()
+
+'''
+{'result_index': 0, 'results': [{'final': False, 'alternatives': [{'transcript': 'hello '}]}], 'warnings': ['Unknown URL query params: access_token. Websockets requests should have the
+parameters in WS messages, not in URL.', 'Unknown arguments: x-watson-learning-learning-opt-out, low_latency.']}
+'''
+
+class PrinterProducer(fluteline.Consumer):
+	def consume(self,item):
+		if 'results' in item:
+			# print(type(item['results']))
+			if item['results'][0]['final']:
+				text = item['results'][0]['alternatives'][0]['transcript']
+				self.output.put(text)
+				print(text)
+
+
+class ViolationChecker(fluteline.Consumer):
+	def enter(self):
+		self.script = ret_qs()
+		print("Script has been processed!")
+	def consume(self,item):
+		# print(self.script)
+		print("Consuming sentence")
+		sentence = read_tokenize_return(item)
+		print(sentence)
+		inter = self.script.intersection(sentence)
+		if len(inter):
+			print("Violation!!!!!")
+			print("Detected the matches: ", inter)
+		
+
+
+def startSTT():
+	settings = {
+		'inactivity_timeout': -1,  # Don't kill me after 30 seconds
+		'interim_results': True,
+	}
+
+# watson_streaming.Transcriber(settings, apikey='88PYNfB8kng7I4uKF2D8OLub0V05-SYmDIA1_gySDEbb', hostname='gateway-wdc.watsonplatform.net'),
+	nodes = [
+		watson_streaming.utilities.MicAudioGen(),
+		watson_streaming.Transcriber(settings, apikey='D8V1hwcYbr_F-EW6m2fddTvQX3kQ8s99D1G8cDwbIExV', hostname='us-east', instance_id='c72b4fe4-66b0-4788-a795-1387e5595e5e'),
+		PrinterProducer(),
+		ViolationChecker(),
+	]
+
+	fluteline.connect(nodes)
+	fluteline.start(nodes)
+
+	try:
+		while True:
+			time.sleep(10)
+	except KeyboardInterrupt:
+		pass
+	finally:
+		fluteline.stop(nodes)
+
+def main():
+	startSTT()
+
+if __name__ == '__main__':
+	main()
